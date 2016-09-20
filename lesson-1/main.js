219bdf6c71deda2946319573ac212c9dc5b8be48
wojciechh-kainos <w.hnatyszyn@kainos.com>
2016-09-20 13:54:07 +0200

    Add code to generate Pascal's triangle in a div.

---
+++ lesson-1/main.js
@@ -0,0 +1,32 @@
+function runScript() {
+    var triangle = [];
+
+    for(var row = 1; row <= 10; row++) {
+
+        triangle[row-1] = [];
+
+        for(var index = 1; index <= row; index++) {
+            triangle[row - 1][index - 1] = getPascalNumber(row, index);
+        }
+    }
+
+    displayTriangle(triangle);
+
+}
+
+function getPascalNumber(row, index) {
+    if(index === 1) return 1;
+    if(index >= row) return 1;
+
+    return getPascalNumber(row - 1, index - 1) + getPascalNumber(row - 1, index);
+}
+
+function displayTriangle(triangle) {
+    var result = "";
+
+    for(var row = 0; row < triangle.length; row ++) {
+        result = result.concat(triangle[row].join("&nbsp;")).concat("<br>");
+    }
+
+    document.getElementById("content").innerHTML = result;
+}
